7a56dcd3cda00addf64a771388e801270d8fd50f
Hyalen Moreira Caldeira <hyalen.moreira@gmail.com>
2018-07-08 23:25:58 -0700

    Add promise support

--- services/contact-app/contact.controller.js
+++ services/contact-app/contact.controller.js
@@ -3,12 +3,33 @@
     app.controller("contactController", contactController);
 
     function contactController(contactDataServiceSrvc) {
-        this.contacts = contactDataServiceSrvc.contacts;
+        var that = this;
 
-        this.selectedContact = this.contacts[0];
+        this.editMode = false;
+
+        contactDataServiceSrvc.getContacts()
+            .then(function (data) {
+                that.contacts = data;
+            });
 
         this.selectContact = function(index) {
+            that.successMessage = undefined;
             this.selectedContact = this.contacts[index];
+        }
+
+        this.toggleEditMode = function() {
+            this.editMode = !this.editMode;
+        }
+
+        this.saveUser = function() {
+            this.toggleEditMode();
+
+            var userData = this.selectedContact;
+
+            contactDataServiceSrvc.saveUser(userData)
+                .then(function () {
+                    that.successMessage = "Data succesfully updated ...";
+                });
         }
     }
 })();
